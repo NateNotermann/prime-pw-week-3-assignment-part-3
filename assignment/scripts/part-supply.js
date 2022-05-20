56d0db2c58de3b3390ba51cfbc915440fa433273
Nate Notermann <nate.notermann@gmail.com>
2022-05-19 20:15:21 -0500

    finished 1, created varaible

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -4,6 +4,9 @@ console.log('****** Part Supply *******');
 // 1. Create a variable called 'partsNeeded' with a value of the number 40 
 //    & console.log the variable
 console.log('1. Number of partsNeeded:');
+let partsNeeded = 40; //this works
+partsNeeded = 40; //or this work, ONLY IF it wasn't previously created in the doc
+console.log( partsNeeded );
 
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
