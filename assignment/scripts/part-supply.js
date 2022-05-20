5d2b0450c51213eb8b7b61a1622fddac629f9a84
Nate Notermann <nate.notermann@gmail.com>
2022-05-19 20:24:05 -0500

    finished 3 console.logged second item in array

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -18,6 +18,9 @@ console.log( 'supplyChanges equals:'+ supplyChanges );
 
 // 3. Console log the value of the second item in the 'supplyChanges' array
 console.log('3. Second supplyChange is:');
+console.log( 'the second item in supplyChanges is:', supplyChanges[1] );  //main way i would do it
+console.log( 'the second item in supplyChanges is: '+ supplyChanges[1] ); //alternative if item needed to be a string
+
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
